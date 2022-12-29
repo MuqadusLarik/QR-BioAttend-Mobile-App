088716b90073d9668fe451999f9611ec4ed39f26
Abdul Sami <thesamishaikh@gmail.com>
2022-12-29 17:24:40 +0500

    Added subject in teachers

--- app/src/main/java/com/techsam/attendencesystemuser/admin/RegisterTeacher.java
+++ app/src/main/java/com/techsam/attendencesystemuser/admin/RegisterTeacher.java
@@ -2,6 +2,8 @@ package com.techsam.attendencesystemuser.admin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,21 +12,50 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.techsam.attendencesystemuser.R;
+import com.techsam.attendencesystemuser.Screens.Subjects;
+import com.techsam.attendencesystemuser.adapters.AddSubjectRecyclerAdapter;
+import com.techsam.attendencesystemuser.adapters.SubjectRecyclerAdapter;
+import com.techsam.attendencesystemuser.objects.Subject;
 import com.techsam.attendencesystemuser.objects.Teacher;
 
-public class RegisterTeacher extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class RegisterTeacher extends AppCompatActivity {
+    RecyclerView recyclerView;
+    AddSubjectRecyclerAdapter adapter;
+    ArrayList<Subject> list;
+    public static ArrayList<Subject> checkBoxList;
     EditText editText,editText2,editText3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_teacher);
+
+        recyclerView = findViewById(R.id.addsubjectrecycler);
+        list = new ArrayList<>();
+        checkBoxList = new ArrayList<>();
         editText = findViewById(R.id.teachername);
         editText2 = findViewById(R.id.teacherusername);
         editText3 = findViewById(R.id.teacherpassword);
+
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        db.child("Subjects").get().addOnCompleteListener(task -> {
+            for(DataSnapshot snap:task.getResult().getChildren()){
+                if(snap.exists()){
+                    Subject subject = snap.getValue(Subject.class);
+                    list.add(subject);
+
+                }
+            }
+            adapter = new AddSubjectRecyclerAdapter(list, RegisterTeacher.this);
+            LinearLayoutManager llm = new LinearLayoutManager(RegisterTeacher.this, RecyclerView.VERTICAL,false);
+            recyclerView.setLayoutManager(llm);
+            recyclerView.setAdapter(adapter);
+        });
 
 
     }
@@ -37,16 +68,19 @@ public class RegisterTeacher extends AppCompatActivity {
 
 
 
+        DatabaseReference db;
+        db= FirebaseDatabase.getInstance().getReference();
+        String id = db.child("Teachers").push().getKey();
         Teacher teacher = new Teacher();
         teacher.setName(name);
         teacher.setUser(username);
         teacher.setPass(password);
+        teacher.setId(id);
+        teacher.setList(checkBoxList);
+;
 
-        DatabaseReference db;
-        db= FirebaseDatabase.getInstance().getReference();
 
-
-        db.child("Teachers").push().setValue(teacher).addOnCompleteListener(new OnCompleteListener<Void>() {
+        db.child("Teachers").child(id).setValue(teacher).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 editText.setText("");
